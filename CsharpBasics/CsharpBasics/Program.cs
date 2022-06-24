68c3d04587de2f415c3895f597c0450f5e570dd4
shyan <shyanwasi@outlook.com>
2022-06-24 17:48:18 +0530

    here is my git

---
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpBasics
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            Console.WriteLine("i love u ");
+            Console.ReadLine();
+        }
+     
+    }
+}
